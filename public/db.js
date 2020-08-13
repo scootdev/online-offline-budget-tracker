fd1d153ecfbd07d3aff666ec19e45f9a980d1aa5
Scott Harris <scottsharris@yahoo.com>
2020-08-13 13:08:49 -0700

    created db.js

---
+++ public/db.js
@@ -0,0 +1,52 @@
+let db;
+const request = indexedDB.open("budget", 1);
+
+request.onupgradeneeded = (e) => {
+    const db = e.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+};
+
+request.onsuccess = (e) => {
+    db = e.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = (e) => {
+    console.log(`error: ${e.target.errorCode}`);
+};
+
+const saveRecord = record => {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    store.add(record);
+};
+
+const checkDatabase = () => {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+
+    getAll.onsuccess = () => {
+        if (getAll.result.length > 0) {
+            fetch("/api/transaction/bulk", {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+                .then(res => res.json())
+                .then(() => {
+                    const transaction = db.transaction(["pending"], "readwrite");
+                    const store = transaction.objectStore("pending");
+                    store.clear();
+                });
+        }
+    };
+}
+
+window.addEventListener("online", checkDatabase);
